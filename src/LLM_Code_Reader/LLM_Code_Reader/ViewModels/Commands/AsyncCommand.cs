68e0893d4f242a413f0ccc44bffc715ee9c28693
Taechuk <XxDarkxX21@outlook.com>
2026-06-02 23:33:52 -0400

    setup relaycommand

---
+++ src/LLM_Code_Reader/LLM_Code_Reader/ViewModels/Commands/AsyncCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LLM_Code_Reader.ViewModels.Commands
+{
+    public class AsyncCommand : ICommand
+    {
+        private readonly Func<object?, Task> _execute;
+        private readonly Func<object?, bool>? _canExecute;
+
+
+        public AsyncCommand(Func<object?, Task> execute, Func<object?, bool>? canExecute)
+        {
+            _execute = execute ?? throw new NullReferenceException("execute");
+            _canExecute = canExecute;
+        }
+
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute.Invoke(parameter);
+        }
+
+
+        public async void Execute(object? parameter)
+        {
+            await _execute.Invoke(parameter);
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public async Task ExecuteAsync(object? parameter)
+        {
+            await _execute.Invoke(parameter);
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
---
+++ src/LLM_Code_Reader/LLM_Code_Reader/ViewModels/Commands/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LLM_Code_Reader.ViewModels.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?> _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action<object?> execute, Predicate<object> predicate)
+        {
+            _execute = execute ?? throw new ArgumentNullException("Parameter can't be null");
+            _canExecute = predicate;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
